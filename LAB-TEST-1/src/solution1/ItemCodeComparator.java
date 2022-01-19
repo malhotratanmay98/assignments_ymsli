14cec836545a15bcbf9347067e97ee775a862e40
Tanmay Malhotra <VE00YM342@YMI.COM>
2022-01-19 16:08:56 +0530

    LAB-TEST-1 Uploaded

---
+++ LAB-TEST-1/src/solution1/ItemCodeComparator.java
@@ -0,0 +1,13 @@
+package solution1;
+
+import java.util.Comparator;
+
+public class ItemCodeComparator implements Comparator<Merchandise>{
+
+	@Override
+	public int compare(Merchandise m1, Merchandise m2) {
+		return m1.getItemCode().compareTo(m2.getItemCode());
+		
+	}
+
+}
